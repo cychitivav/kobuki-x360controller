6a189574f8611e7b01f791ee7e7a265e83cb9b22
Cristian Chitiva <30636259+cychitivav@users.noreply.github.com>
2021-04-08 21:55:13 -0500

    Controller as class

--- scripts/xbox_controller.py
+++ scripts/xbox_controller.py
@@ -6,50 +6,52 @@ from geometry_msgs.msg  import Twist
 import numpy as np
 
 
-def publisher_x360():
-    while not rospy.is_shutdown():
-        try:
-            with Xbox360Controller(0, axis_threshold=0) as controller:
-                # Left and right axis move event
-                controller.axis_l.when_moved = on_axis_moved
-                controller.axis_r.when_moved = on_axis_moved
-                
-                # Button stop
-                controller.button_a.when_released = on_button_released
-
-                signal.pause()
-        except Exception:
-            pass
-
-def on_axis_moved(axis):
-    while not rospy.is_shutdown():
-        msg = Twist()
-
-        if axis.name == "axis_l":
-            msg.linear.x = axis.y
-            msg.linear.y = axis.x
-
-        if axis.name == "axis_r":
-            msg.angular.z = axis.x
-
-        pub.publish(msg)
-        print(msg)
-
-def on_button_released(button):
-    while not rospy.is_shutdown():
-        rospy.loginfo("Stopping")
-        msg = Twist()
+class controller():
+    def __init__(self):
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         
-        pub.publish(msg)
+        while not rospy.is_shutdown():
+            try:
+                with Xbox360Controller(0, axis_threshold=0.2) as controller:
+                    # Left and right axis move event
+                    controller.axis_l.when_moved = on_axis_moved
+                    controller.axis_r.when_moved = on_axis_moved
 
-        rospy.loginfo("Shutting down")
-        rospy.signal_shutdown("Request shutdown")
+                    # Button stop
+                    controller.button_a.when_released = on_button_released
+
+                    signal.pause()
+            except Exception:
+                pass
+
+    def on_axis_moved(self, axis):
+        while not rospy.is_shutdown():
+            msg = Twist()
+
+            if axis.name == "axis_l":
+                msg.linear.x = axis.y
+                msg.linear.y = axis.x
+
+            if axis.name == "axis_r":
+                msg.angular.z = axis.x
+
+            sel.pub.publish(msg)
+            print(msg)
+
+    def on_button_released(self, button):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Stopping")
+            msg = Twist()
+
+            self.pub.publish(msg)
+
+            rospy.loginfo("Shutting down")
+            rospy.signal_shutdown("Request shutdown")
 
 if __name__ == "__main__":
     rospy.init_node("x360_controller", anonymous=True) 
     
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    publisher_x360()     
+    controller()     
     rospy.spin()
 
 
